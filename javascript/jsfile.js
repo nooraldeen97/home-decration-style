f0fb4853bd4ba7bd0c5ced0354504b26e93b56df
nooraldeen97 <noor-aldeen97@hotmail.com>
2021-03-21 15:21:45 +0200

    i added while and for loops

--- javascript/jsfile.js
+++ javascript/jsfile.js
@@ -9,6 +9,8 @@ document.write('<h2>'+ "we recommend you to choose the cottage decoration"+'<h2>
 //console.log(house)
 document.write(house)
 
+
+
 if(house==1){alert("we can help you also decorate your backyard")
 }else{
     alert("your appartment will be nice by working with us")
@@ -19,3 +21,30 @@ if (confirm('Are you sure you want tthese service?')) {
   } else {
       alert("we hope you to choose on of our service")
   }
+
+
+
+  var need = prompt("what kind of service do you need? build/maintainance");
+  while(need !="Maintainance" && need != "Build")
+  {
+      alert("please write 'Build' fot building service and 'Maintainance' for maintainance service.")
+      need = prompt("what kind of service do you need? ")
+  }
+
+
+
+  var nomberOfAppartment = prompt("how many appartment do you want to build");
+
+  var appartment ='';
+  var result ='';
+
+  if(need=="Build"){
+      appartment=' <img src="https://cf.bstatic.com/images/hotel/max1024x768/217/217203339.jpg" alt="cottage pic">'
+  } else if (need=="Maintainance") {
+      appartment='<img src="https://i.pinimg.com/564x/29/61/2f/29612fc4db79125ee7c81910e1cace1f.jpg" alt="appartment pic">'
+  }
+
+for(var  i=0 ; i < nomberOfAppartment ; i++ ){
+    result+=appartment;
+}
+document.write(result);
